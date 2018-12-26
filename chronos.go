5b3cc8d342c5f03ebbea31e9146e3968bc95872e
Vaclav Rozsypalek <vaclav@giantswarm.io>
2018-12-26 16:01:44 +0100

    init

---
+++ chronos.go
@@ -0,0 +1,44 @@
+package chronos
+
+import (
+	"fmt"
+	"time"
+)
+
+type TimeInterval struct {
+	Start time.Time
+	End   time.Time
+}
+
+func New() *TimeInterval {
+	return &TimeInterval{
+		Start: time.Now(),
+		End:   nil,
+	}
+}
+
+func (t *TimeInterval) Finish() {
+	t.End = time.Now()
+}
+
+func (t *TimeInterval) StringSec() string {
+	if t.End == time.Time(nil) {
+		return "not_finished"
+	}
+	return fmt.Sprintf("%.2f", t.End.Sub(t.Start).Seconds())
+}
+
+func (t *TimeInterval) StringMilisec() string {
+	if t.End == time.Time(nil) {
+		return "not_finished"
+	}
+	return fmt.Sprintf("%d", t.End.Sub(t.Start).Nanoseconds()/int64(time.Millisecond))
+}
+
+func (t *TimeInterval) String() string {
+	if t.End == time.Time(nil) {
+		return "not_finished"
+	}
+	return fmt.Sprintf("%s", t.End.Sub(t.Start).String())
+
+}
